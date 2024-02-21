e32fa435ef9a12e7f60aa3260126f7a360405ca4
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2024-02-21 15:18:10 -0330

    --status checks for directories

--- commands/init.js
+++ commands/init.js
@@ -1,11 +1,29 @@
+const fs = require("fs");
+const fsPromises = fs.promises;
+const path = require("path");
+
 const init = (userArgs) => {
   // Different functionality depending on the flags issued
   switch (userArgs[1]) {
+    case "--status":
+      break;
     default:
       console.log(
-        `'${userArgs.join(" ")}' is not a valid command. Try 'init --help'`
+        `'${userArgs.join()}' is not a valid command. Try 'init --help'`
       );
   }
+};
+
+// init --status
+const initStatus = () => {
+  let status = "";
+  status +=
+    fs.existsSync(path.join(__dirname, "logs")) &&
+    fs.existsSync(path.join(__dirname, "help"))
+      ? "Directories are initialized. "
+      : "Directories are NOT initialized. ";
+
+  console.log(status);
 };
 
 module.exports = init;
