6cfaf2ec38f7b88a1d90fb06eaace79009a1f28b
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2024-02-22 20:24:55 -0330

    Duplicate users cannot be created; Users can be modified with --upd

--- commands/tokenBit.js
+++ commands/tokenBit.js
@@ -35,12 +35,22 @@ const token = (userArgs) => {
           console.log(`There are currently ${users.length} users.`);
           break;
 
+        // New user command
         case "--new":
+          // Function which searches the provided user list for a given username
+          const getIndexByUser = (username) => {
+            // -1 is returned when findIndex() cannot find a match
+            return users.findIndex((user) => user.username == username);
+          };
+
           // Ensure additional options are specified
           if (userArgs[2] == undefined)
             console.log(
               "A username must be specified. Use 'bitbase token --new <username>'."
             );
+          // Check if a user by the given name exists before continuing
+          else if (getIndexByUser(users, userArgs[2]) > -1)
+            console.log(`A user named ${userArgs[2]} already exists.`);
           else {
             // Generate a new token hash
             const hash = crypto
@@ -67,9 +77,52 @@ const token = (userArgs) => {
           }
           break;
 
+        // Update user command
+        case "--upd":
+          // Ensure additional options are specified
+          if (
+            userArgs[2] == undefined ||
+            userArgs[3] == undefined ||
+            userArgs[4] == undefined
+          )
+            console.log(
+              "Additional options must be specified. Try 'bitbase token --help"
+            );
+          else {
+            let userIndex = getIndexByUser(users, userArgs[3]);
+            // If the user doesn't exist, show a message
+            if (userIndex == -1)
+              console.log(
+                `Cannot update user ${userArgs[3]}, as it does not exist.`
+              );
+            else {
+              if (userArgs[2] == "e") {
+                // Set the user's email value
+                users[userIndex].email = userArgs[4];
+                // Save the updated user data
+                fs.writeFileSync(userFile, JSON.stringify(users));
+
+                console.log(`User '${userArgs[3]}' email has been updated.`);
+              } else if (userArgs[2] == "p") {
+                // Set the user's phone number value
+                users[userIndex].phoneNum = userArgs[4];
+                // Save the updated user data
+                fs.writeFileSync(userFile, JSON.stringify(users));
+
+                console.log(
+                  `User '${userArgs[3]}' phone number has been updated.`
+                );
+              } else
+                console.log(
+                  `'${userArgs.join()}' is not a valid command. Try 'bitbase token --help'`
+                );
+            }
+          }
+          break;
+
         default:
           console.log(
-            `'${userArgs.join()}' is not a valid command. Try 'token --help'`
+            `'${userArgs.join()}' is not a valid command. Try 'bitbase token --help'`
           );
       }
     } catch (e) {
